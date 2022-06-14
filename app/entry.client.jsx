c7bd01e377363e7e05b0d3e6843c0a495e2b7d41
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-06-14 20:12:24 +0530

    Remove prefetcher in window

--- app/entry.client.jsx
+++ app/entry.client.jsx
@@ -1,26 +1,7 @@
 import { hydrate } from 'react-dom'
 import { RemixBrowser } from 'remix'
-import { install as installSW, prefetch } from '@layer0/prefetch/window'
+import { install } from '@layer0/prefetch/window'
 
 hydrate(<RemixBrowser />, document)
 
-installSW({
-  watch: [
-    {
-      selector: 'a',
-      callback: (el) => {
-        if (!el.href.includes('https://')) {
-          prefetch(el.href)
-        }
-      },
-    },
-    {
-      selector: 'img',
-      callback: (el) => {
-        if (!el.src.includes('https://')) {
-          prefetch(el.src)
-        }
-      },
-    },
-  ],
-})
+install()
