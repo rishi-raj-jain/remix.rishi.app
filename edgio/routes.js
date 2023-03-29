1b61f83cfe199bcd1dc8db1ba7dc8366270a48a5
Rishi Raj Jain <rjain@llnw.com>
2023-03-29 17:34:55 +0530

    cache revalidate forever

--- edgio/routes.js
+++ edgio/routes.js
@@ -4,6 +4,23 @@ const router = new Router()
 
 router.static('public')
 
+router.match(
+  {
+    path: '/:path*',
+    query: {
+      _data: /routes%2F.*/,
+    },
+  },
+  ({ cache }) => {
+    cache({
+      edge: {
+        maxAgeSeconds: 1,
+        staleWhileRevalidateSeconds: 60 * 60 * 24 * 365,
+      },
+    })
+  }
+)
+
 router.match('/service-worker.js', ({ serviceWorker }) => {
   serviceWorker('.edgio/temp/service-worker.js')
 })
