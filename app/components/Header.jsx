583049c9b25ac4de3de9e86543bb05910bfdc633
Rishi Raj Jain <rjain@llnw.com>
2023-03-29 17:49:15 +0530

    prefetch content

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -7,19 +7,19 @@ export default function Header() {
       <div className="flex w-full max-w-[90vw] flex-row items-center justify-between sm:px-10 lg:max-w-[75vw]">
         <Toggle />
         <div className="relative flex max-w-[258px] flex-row items-center space-x-5 overflow-x-scroll sm:max-w-none sm:overflow-x-hidden">
-          <Link className="dark:text-white" to="/">
+          <Link prefetch="intent" className="dark:text-white" to="/">
             Home
           </Link>
-          <Link className="dark:text-white" to="/about">
+          <Link prefetch="intent" className="dark:text-white" to="/about">
             About
           </Link>
-          <Link className="dark:text-white" to="/blogs">
+          <Link prefetch="intent" className="dark:text-white" to="/blogs">
             Blogs
           </Link>
-          <Link className="dark:text-white" to="/cv">
+          <Link prefetch="intent" className="dark:text-white" to="/cv">
             CV
           </Link>
-          <Link className="dark:text-white" to="/storyblok">
+          <Link prefetch="intent" className="dark:text-white" to="/storyblok">
             Storyblok
           </Link>
         </div>
